e1e14dd21beff6bfc13af8fbcca7bee00880b67f
Amanda Mattleman <amandamattleman@MSI>
2021-04-21 10:00:36 -0600

    project start

---
+++ src/Components/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Home extends Component{
+
+   render(){
+       return(
+           <div><h1>I'm Amanda White, and this is my portfolio site! Welcome!</h1></div>
+       )
+   }
+}
+export default Home;
